d37a1d451eeeb5975a36f7be1820fe956d750497
Andrew Chou <andrewchou1996@gmail.com>
2023-02-19 10:05:05 -0800

    Add withAuth middleware

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 // {==================== Route to get all posts | GET [http://localhost:3001/] | hompage.handlebars ====================}
 router.get("/", async (req, res) => {
@@ -55,48 +56,60 @@ router.get("/post/:id", async (req, res) => {
 });
 
 // {==================== Route to get user's dashboard | GET [baseUrl/dashboard] | dashboard.handlebars ====================}
-// Use withAuth middleware to prevent access to route.....
-router.get("/dashboard", async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Post }],
-    });
+router.get(
+  //  <---------------- route path ---------------->
+  "/dashboard",
+  //  <---------------- withAuth middleware to prevent access to route | redirect them to the login route ---------------->
+  withAuth,
+  //  <---------------- call back to get user and render page dashboard.handlebars ---------------->
+  async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ["password"] },
+        include: [{ model: Post }],
+      });
 
-    const user = userData.get({ plain: true });
+      const user = userData.get({ plain: true });
 
-    // res.status(200).json(user);
-    res.render("dashboard", {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+      // res.status(200).json(user);
+      res.render("dashboard", {
+        ...user,
+        logged_in: true,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 // {==================== Route to get a single post with modified form (delete or update) | GET [baseUrl/dashboard/post/:id] | modify-post.handlebars ====================}
-// Use withAuth middleware to prevent access to route....
-router.get("/dashboard/post/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        { model: Comment, include: [{ model: User }] },
-        { model: User },
-      ],
-    });
-    if (!postData) {
-      res.status(404).json({ message: "No post with this id!" });
-      return;
-    }
-    const post = postData.get({ plain: true });
+router.get(
+  //  <---------------- route path ---------------->
+  "/dashboard/post/:id",
+  //  <---------------- withAuth middleware to prevent access to route | redirect them to the login route ---------------->
+  withAuth,
+  //  <---------------- call back to get post and render modify-post.handlebars page ---------------->
+  async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          { model: Comment, include: [{ model: User }] },
+          { model: User },
+        ],
+      });
+      if (!postData) {
+        res.status(404).json({ message: "No post with this id!" });
+        return;
+      }
+      const post = postData.get({ plain: true });
 
-    res.render("modify-post", { ...post, logged_in: req.session.logged_in });
-  } catch (err) {
-    res.status(500).json(err);
+      res.render("modify-post", { ...post, logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 // {==================== Route to get login form | GET [baseUrl/login] | login.handlebars ====================}
 router.get("/login", (req, res) => {
